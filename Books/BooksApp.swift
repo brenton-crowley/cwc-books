21629c111a0f33d31d71523dff237ed5799568e9
Brent <brenton.crowley@gmail.com>
2022-02-24 16:30:48 +1100

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Brent on 24/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
